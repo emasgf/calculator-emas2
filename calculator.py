d32b27841d029a8fde808517b31bde677dccc235
masabem2 <emmanuel.masabo@cegepoutaouais.qc.ca>
2026-07-27 00:04:11 -0400

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+if __name__ == "__main__":
+    print(add(5, 3))
+    print(subtract(5, 3))
+    print(multiply(5, 3))
